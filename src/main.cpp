8bc9c9bee828502a344444131f609b2f4db5e74b
Santiago Moliner <moliner.santiago@gmail.com>
2025-10-25 22:18:29 +0200

    Example for register, update and unregister

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,75 @@
+#include <chrono>
 #include <sol/sol.hpp>
-#include <iostream>
 
-int main() {
-  sol::state lua;
-  lua.open_libraries(sol::lib::base);
+int main()
+{
 
-  // Bind a function
-  lua.set_function("cpp_hello", [](const std::optional<std::string>& arg)
-  {
-    if (arg.has_value()) {
-      std::cout << "Hello from C++! " << arg.value() << std::endl;
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::string);
+
+    sol::load_result chunk = lua.load_file("/home/sanmopre/development/lua_project_template/src/scripts/script.lua");
+    if (!chunk.valid())
+    {
+        const sol::error err = chunk;
+        std::cerr << "Failed to load script.lua: " << err.what() << "\n";
+        return 1;
     }
-  });
 
-  // Execute Lua code
-  lua.script(R"(
-        print("Hello from Lua!")
-        cpp_hello("assa")
-    )");
+    if ( const sol::protected_function_result exec = chunk(); !exec.valid())
+    {
+        const sol::error err = exec;
+        std::cerr << "Error running script.lua: " << err.what() << "\n";
+        return 1;
+    }
 
-  return 0;
+    const sol::protected_function registerFunction = lua["register"];
+    if (!registerFunction.valid())
+    {
+        std::cerr << "Lua function 'register' not found.\n";
+        return 1;
+    }
+
+    const sol::protected_function unregisterFunction = lua["unregister"];
+    if (!unregisterFunction.valid())
+    {
+        std::cerr << "Lua function 'unregister' not found.\n";
+        return 1;
+    }
+
+
+    const sol::protected_function updateFunction = lua["update"];
+    if (!updateFunction.valid())
+    {
+        std::cerr << "Lua function 'update' not found.\n";
+        return 1;
+    }
+
+    std::ignore = registerFunction();
+
+    bool running = true;
+    double dt = 0.0;
+    while (running)
+    {
+        // Record start time
+        auto start = std::chrono::steady_clock::now();
+
+        sol::protected_function_result result = updateFunction(dt);
+        if (!result.valid())
+        {
+            const sol::error err = result;
+            std::cerr << "Error calling update: " << err.what() << "\n";
+            return 1;
+        }
+
+        std::chrono::duration<double> elapsed_seconds = std::chrono::steady_clock::now() - start;
+        dt = elapsed_seconds.count();
+
+        running = result;
+
+
+    }
+
+    std::ignore = unregisterFunction();
+
+    return 0;
 }
--- src/scripts/script.lua
+++ src/scripts/script.lua
@@ -1,4 +1,18 @@
-print("Hello from Lua!")
-for i = 1, 5 do
-    print("Lua counting:", i)
+function register()
+    print("register!")
+end
+
+executionTime = 0.0
+
+function update(dt)
+    print("updating with execution time of: ", executionTime)
+    executionTime = executionTime + dt
+
+    if executionTime < 5.0 then return true end
+
+    return false
+end
+
+function unregister()
+    print("unregister!")
 end
